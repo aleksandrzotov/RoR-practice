2b03e67808507defecd286a07a265caf33d4848b
Aleksandr Zotov <aleksandr@speedsize.com>
2023-04-21 14:01:32 +0700

    add Post CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,36 @@
 class PostsController < ApplicationController
 
+    def new
+    end
+
+    def create
+        Post.create(
+            text: params[:posts][:text],
+            user: current_user)
+        redirect_to current_user
+    end
+
     def index
         @posts = Post.all
     end
 
     def show
-        @posts = Post.find(params[:id])
+        @post = Post.find(params[:id])
+    end
+
+    def destroy
+        Post.delete(params[:id])
+        redirect_to current_user
+    end
+
+    def edit
+        puts(params)
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:id])
+        @post.update(text: params[:post][:text])
+        redirect_to current_user
     end
 end
