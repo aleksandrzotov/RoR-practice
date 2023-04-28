e6d6cdf45d256cf12bfe26caed8c967b55a2ebce
Aleksandr Zotov <aleksandr@speedsize.com>
2023-04-28 12:33:11 +0700

    fix user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,28 @@ class UsersController < ApplicationController
     def show
       @user = User.includes(:posts).find(params[:id])
     end
+
+    def new
+      @user = User.new
+    end
+
+  def create
+    @user = User.new(user_params)
+
+    @user.email.downcase!
+
+    if @user.save
+      flash[:notice] = "Account created successfully!"
+      redirect_to root_path
+    else
+      flash.now.alert = "Oops, couldn't create account. Please make sure you are using a valid email and password and try again."
+      render :new
+    end
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
