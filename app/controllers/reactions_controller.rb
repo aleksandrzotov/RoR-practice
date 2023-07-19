8e972052cd2ae8b96d30683ecc4ab42e3d38c2ec
Aleksandr Zotov <aleksandr@speedsize.com>
2023-07-19 11:35:38 +0700

    add reactions endpoint to api

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -1,8 +1,13 @@
 class ReactionsController < ApplicationController
   def create
-    @kind = params[:kind]
-    @post = Post.find(params[:post_id])
-    Reaction.create(kind: params[:kind], post: @post, user: current_user)
-    @post.reload
+    if !current_user
+      flash.notice = "not permited"
+      redirect_to root_path
+    else
+      @kind = params[:kind]
+      @post = Post.find(params[:post_id])
+      Reaction.create(kind: params[:kind], post: @post, user: current_user)
+      @post.reload
+    end
   end
 end
